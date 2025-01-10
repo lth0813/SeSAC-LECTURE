be1cf525e7ef770b8eceb73dbc120d747d84dab0
lth0813 <lth0813@gmail.com>
2025-01-10 17:22:59 +0900

    250110

---
+++ 개인 실습/my_module2.py
@@ -0,0 +1,5 @@
+# File name: my_module2.py
+def func2():
+    print("func2 in my_module2 ")
+def func3():
+    print("func3 in my_module2 ")
---
+++ 개인 실습/my_module_test1.py
@@ -0,0 +1,4 @@
+# File name: my_module_test1.py
+def func(a):
+    print("입력 숫자:", a)
+func(3)
