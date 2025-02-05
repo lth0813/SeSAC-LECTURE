e5b648d911a4e5d86f0f2ceb6ab66fd4c754480e
lth0813 <lth0813@gmail.com>
2025-02-05 17:33:16 +0900

    250205 2nd

--- 개인 실습/flaskbook/apps/minimalapp/app.py
+++ 개인 실습/flaskbook/apps/minimalapp/app.py
@@ -21,6 +21,8 @@ app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
 app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
 
+print(app.config["MAIL_USERNAME"])
+
 mail = Mail(app)
 
 toolbar = DebugToolbarExtension(app)
