efb1bff30f11c4b54b3c9618897e3ffea28df031
lth0813 <lth0813@gmail.com>
2025-02-13 17:16:39 +0900

    250213 1st

--- 과제/MongoDB/app.py
+++ 과제/MongoDB/app.py
@@ -4,20 +4,24 @@ import gridfs
 import os
 from bson import ObjectId
 import io
+import subprocess
+import imghdr
 
 class HandleImage():
     def __init__(self, db_name="image_db", collection_name="fs"):
         self.client = MongoClient("mongodb://localhost:27017/")
         self.db = self.client[db_name]
         self.fs = gridfs.GridFS(self.db, collection=collection_name)
+        subprocess.Popen(r' start cmd /k "mongod --dbpath C:/mongodb/cats"', shell=True)
 
     def save_image(self, file):
-        file_id = self.fs.put(file.read(), filename = file.filename)
+        order = self.db.fs.files.count_documents({}) + 1
+        file_id = self.fs.put(file.read(), filename = file.filename, metadata={"order": order})
         return str(file_id)
     
     def get_image(self):
         images = []
-        for file in self.db.fs.files.find():
+        for file in self.db.fs.files.find().sort("metadata.order"):
             images.append({
                 "filename" : file["filename"],
                 "upload_date" : file["uploadDate"].strftime('%Y-%m-%d'),
@@ -27,16 +31,26 @@ class HandleImage():
         return images
     
     def delete_image(self, file_id):
+        file_id = ObjectId(file_id)
         self.fs.delete(file_id)
 
     def update_image(self, file_id, new_file):
-        self.delete_image(file_id)
-        new_file_id = self.save_image(new_file)
-        return str(new_file_id)
+        file_id = ObjectId(file_id)
+        origin_file = self.db.fs.files.find_one({"_id": file_id})
+        metadata = origin_file.get("metadata", {})
+        with self.client.start_session() as session:
+            with session.start_transaction():
+                self.fs.delete(file_id)
+                new_file_id = self.fs.put(new_file.read(), filename = new_file.filename, metadata = metadata, _id = file_id)
+                return str(new_file_id)
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 handler = HandleImage()
+
+@app.route("/")
+def initserver():
+    return render_template("init.html")
 
 @app.route("/imageserver")
 def imagesever():
@@ -45,11 +59,29 @@ def imagesever():
 
 @app.route("/imageserver/insert", methods=["POST"])
 def insert_image():
-    file = request.files["file"]
+    file = request.files["insert_file"]
     handler.save_image(file)
     return redirect("/imageserver")
 
 @app.route("/imageserver/get/<file_id>")
 def image_get(file_id):
     file = handler.fs.get(ObjectId(file_id))
-    return send_file(io.BytesIO(file.read()), mimetype="image/jpg")
+    file_data = file.read()
+    img_type = imghdr.what(None, file_data)
+    if img_type:
+        mimetype = f"image/{img_type}"
+    else:
+        mimetype = "application/octet-stream"
+    return send_file(io.BytesIO(file_data), mimetype=mimetype)
+
+@app.route("/imageserver/update", methods=["POST"])
+def update_image():
+    file = request.files["update_file"]
+    file_id = request.form.get("origin_id")
+    handler.update_image(file_id, file)
+    return redirect("/imageserver")
+
+@app.route("/imageserver/delete/<file_id>")
+def delete_image(file_id):
+    handler.delete_image(file_id)
+    return redirect("/imageserver")
--- 과제/MongoDB/static/imageserver.js
+++ 과제/MongoDB/static/imageserver.js
@@ -1,12 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // 이미지 추가 창 출력
   const addBtn = document.querySelector(".add_btns");
   const insertForm = document.querySelector(".insert_form");
-
   addBtn.addEventListener("click", function () {
     if (insertForm.style.display === "flex") {
       insertForm.style.display = "none";
     } else {
       insertForm.style.display = "flex";
     }
+  });
+  // 이미지 업데이트 창 출력
+  const updateBtn = document.querySelector(".update_btn");
+  const updateForm = document.querySelector(".update_form");
+  updateBtn.addEventListener("click", () => {
+    if (updateForm.style.display === "flex") {
+      updateForm.style.display = "none";
+    } else {
+      updateForm.style.display = "flex";
+    }
+  });
+
+  // 모달 창 출력
+  const thumbnails = document.querySelectorAll(".thumbnail");
+  const modal = document.querySelector(".des_modal");
+  const closeBtn = document.querySelector(".close_btn");
+  const container = document.querySelector(".container");
+  const deleteBtn = document.querySelector(".delete_btn");
+
+  thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener("click", function () {
+      const imageData = images[index];
+      document.querySelector(".mimage").src =
+        this.querySelector(".thumbnail_img").src;
+      document.querySelector(".mfilename").textContent = imageData.filename;
+      document.querySelector(".mfliecdate").textContent = imageData.upload_date;
+      document.querySelector(".mfilesize").textContent =
+        imageData.filesize + " bytes";
+      document.querySelector(".mfileid").textContent = imageData.file_id;
+      document.querySelector(".origin_id").value = imageData.file_id;
+
+      modal.style.display = "flex";
+      container.classList.add("blur-background");
+
+      // 이미지 삭제 JS
+      deleteBtn.replaceWith(deleteBtn.cloneNode(true));
+      document.querySelector(".delete_btn").addEventListener("click", () => {
+        location.href = "/imageserver/delete/" + imageData.file_id;
+      });
+    });
+  });
+
+  closeBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+    container.classList.remove("blur-background");
   });
 });
