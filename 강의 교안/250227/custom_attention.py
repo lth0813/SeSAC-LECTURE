8d84dfa952c33a0cb7f1d8e144a10c4ad7ec7031
lth0813 <lth0813@gmail.com>
2025-03-06 18:22:47 +0900

    250306 1st

--- 강의 교안/250227/custom_attention.py
+++ 강의 교안/250227/custom_attention.py
@@ -1,55 +1,65 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Layer, Dense, Concatenate, Activation
+from tensorflow.keras.layers import Layer, Dense, Concatenate, Activation, Dot, RepeatVector, Add
 
 class CustomAttention(Layer):
-    def __init__(self, units=64, score_type='luong', **kwargs):
+    def __init__(self, units=128, score_type='luong', **kwargs):
         super(CustomAttention, self).__init__(**kwargs)
         self.units = units
         self.score_type = score_type
         
     def build(self, input_shape):
         # Luong style
-        self.luong_wa = Dense(input_shape[-1], use_bias=False)
+        input_dim = input_shape[-1]
+        self.luong_w = Dense(input_dim, use_bias=False, name='luong_w')
+        self.luong_dot = Dot(axes=[2, 2], name='luong_dot')
         
         # Bahdanau style
-        self.bahdanau_w1 = Dense(self.units, use_bias=False)
-        self.bahdanau_w2 = Dense(self.units, use_bias=False)
-        self.bahdanau_v = Dense(1, use_bias=False)
+        self.bahdanau_w1 = Dense(input_dim, use_bias=False, name='bahdanau_w1')
+        self.bahdanau_w2 = Dense(input_dim, use_bias=False, name='bahdanau_w2')
+        self.bahdanau_v = Dense(1, use_bias=False, name='bahdanau_v')
+        self.bahdanau_tanh = Activation('tanh', name='bahdanau_tanh')
+        self.bahdanau_add = Add(name='bahdanau_add')
         
-        # Output transformation
-        self.w_c = Dense(self.units, use_bias=False, activation='tanh')
-        
-        # Other layers
-        self.concat = Concatenate()
+        # Attention weight softmax
+        self.softmax_normalizer = Activation('softmax', name='attention_weights')
+
+        # Dot product for context vector
+        self.dot_context = Dot(axes=[2, 1], name='context_vector')
+
+        # Final transformation
+        self.concat = Concatenate(name='concat_c_h')
+        self.w_c = Dense(self.units, use_bias=False, activation='tanh', name='attention_vector')
+
         super(CustomAttention, self).build(input_shape)
         
     def call(self, inputs, **kwargs):
         # Get last hidden state
-        h_t = inputs[:, -1:, :]  # (batch_size, 1, units)
-        h_s = inputs[:, :-1, :]  # (batch_size, time-1, units)
+        h_s = inputs  # (batch_size, time_steps, units)
+        h_t = h_s[:, -1:, :]  # (batch_size, 1, units)
         
         # Calculate attention scores
         if self.score_type == 'luong':
-            # Luong's multiplicative style
-            score = tf.matmul(h_t, self.luong_wa(h_s), transpose_b=True)
+            score = self.luong_dot([h_t, self.luong_w(h_s)])
         else:
-            # Bahdanau's additive style
-            h_t_expanded = tf.tile(h_t, [1, tf.shape(h_s)[1], 1])
-            score = self.bahdanau_v(
-                tf.tanh(self.bahdanau_w1(h_t_expanded) + self.bahdanau_w2(h_s))
-            )
+            a1 = self.bahdanau_w1(h_t)
+            a2 = self.bahdanau_w2(h_s)
+            a1 = RepeatVector(tf.shape(h_s)[1])(a1)
+            score = self.bahdanau_tanh(self.bahdanau_add([a1, a2]))
+            score = self.bahdanau_v(score)
+            score = tf.squeeze(score, axis=-1)
         
-        # Calculate attention weights
-        attention_weights = tf.nn.softmax(score, axis=-1)
-        
-        # Apply attention weights
-        context = tf.matmul(attention_weights, h_s)
-        
+        # Compute attention weights
+        attention_weights = self.softmax_normalizer(score)  # (batch_size, time_steps)
+        attention_weights = tf.expand_dims(attention_weights, axis=1)  # (batch_size, 1, time_steps)
+
+        # Compute context vector
+        context = self.dot_context([attention_weights, h_s])  # (batch_size, 1, units)
+
         # Combine context with last hidden state
-        h_t = tf.squeeze(h_t, axis=1)
-        context = tf.squeeze(context, axis=1)
-        attention_output = self.w_c(self.concat([context, h_t]))
-        
+        h_t = tf.squeeze(h_t, axis=1)  # (batch_size, units)
+        context = tf.squeeze(context, axis=1)  # (batch_size, units)
+        attention_output = self.w_c(self.concat([context, h_t]))  # (batch_size, units)
+
         return attention_output
         
     def compute_output_shape(self, input_shape):
@@ -61,4 +71,4 @@ class CustomAttention(Layer):
             'units': self.units,
             'score_type': self.score_type
         })
-        return config 
+        return config
